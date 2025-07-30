c46e7a8bdae3289aec24661d7f887f98f2409d75
Alexander Abbosh <alex.abbosh@gmail.com>
2025-07-30 13:06:54 +1000

    JavaFx stuff

--- GroupProject/src/main/java/org/example/Main.java
+++ GroupProject/src/main/java/org/example/Main.java
@@ -1,17 +1,38 @@
-package org.example;
+package org.oosd;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        launch(args);
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Please input your name:");
+        TextField input = new TextField();
+        Button btn = new Button("Click Me");
+        StackPane root = new StackPane();
+        root.getChildren().addAll(label, input, btn);
+        btn.setOnAction(e -> {
+            int x = 10;
+            int y = x + 5;
+            System.out.println(x + y);
+            label.setText("Hello, " + input.getText() + "!");
+        });
+        StackPane.setAlignment(label, Pos.TOP_CENTER);
+        StackPane.setAlignment(input, Pos.CENTER);
+        StackPane.setAlignment(btn, Pos.BOTTOM_CENTER);
+        Scene scene = new Scene(root, 400, 200);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("JavaFX Demo");
+        primaryStage.show();
     }
 }
